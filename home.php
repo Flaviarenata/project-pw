e112e12849870d982de8c156073331ea87174a77
Flaviarenata <flaviafitrirenata@gmail.com>
2025-01-15 12:50:33 +0700

    project uas

---
+++ home.php
@@ -0,0 +1,264 @@
+<div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+      WELCOME TO BERKAH JAYA ABADI LIBRARY<br>
+      Selamat Datang di Perpustakaan Kami!
+      <br>Dimana pengetahuan bertemu teknologi.
+      Dimana membaca menjadi lebih mudah dan menyenangkan.
+      Dimana setiap orang bisa mengakses dunia literasi tanpa batas.
+      
+      <br>Mari mulai perjalanan membaca Anda!
+
+      </button>
+    </h2>
+ <div class="accordion-item">
+    <h2 class="accordion-header">
+      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        LIST BUKU
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      <table id="bookList" class="table">
+  <thead>
+    <tr>
+      <th scope="col">NO</th>
+      <th scope="col">Judul Buku</th>
+      <th scope="col">Genre</th>
+      <th scope="col">Penulis</th>
+      <th scope="col">Status</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr data-book="Arjuna & Kirana">
+      <th scope="row">1</th>
+      <td>Arjuna & Kirana</td>
+      <td>Drama</td>
+      <td>Pricia Chang</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Anak-anak Kuala">
+      <th scope="row">2</th>
+      <td>Anak-anak Kuala</td>
+      <td>Fiksi</td>
+      <td>Fajar Mesaz</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Anastasia Diary Teluk Alaska">
+      <th scope="row">3</th>
+      <td>Anastasia Diary Teluk Alaska</td>
+      <td>Petualangan</td>
+      <td>Eka Aryani</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Bulan">
+      <th scope="row">4</th>
+      <td>Bulan</td>
+      <td>romantis</td>
+      <td>Tere Liye</td>
+      <td class="status">Tersedia</td>>
+    </tr>
+    <tr data-book="Bumi">
+      <th scope="row">5</th>
+      <td>Bumi</td>
+      <td>fiksi Ilmiah</td>
+      <td>Tere Liye</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="But I Still Love You">
+      <th scope="row">6</th>
+      <td>But I Still Love you</td>
+      <td>romantis</td>
+      <td>Choye Youn</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Cermin Jiwa">
+      <th scope="row">7</th>
+      <td>Cermin Jiwa</td>
+      <td>Drama</td>
+      <td>Sri Rahayu Ptihatmi</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Chasing Shadows">
+      <th scope="row">8</th>
+      <td>Chasing Shadows</td>
+      <td>Thriller</td>
+      <td>C.J. Lyons</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Cool BadBoy">
+      <th scope="row">9</th>
+      <td>Cool BadBoy</td>
+      <td>Romantis</td>
+      <td>Luluara</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Dalam Sketsa">
+      <th scope="row">10</th>
+      <td>Dalam Sketsa</td>
+      <td>Drama</td>
+      <td>Ikrom Mustofa</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Don't Let Me Go">
+      <th scope="row">11</th>
+      <td>Don't Let Me Go</td>
+      <td>Romantis</td>
+      <td>Kelsie Rae</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Dua Garis Biru">
+      <th scope="row">12</th>
+      <td>Dua Garis biru</td>
+      <td>Drama</td>
+      <td>Lucia Priandarini</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Eplilog Rasa">
+      <th scope="row">13</th>
+      <td>Epilog Rasa</td>
+      <td>Drama</td>
+      <td>Desi Suryani</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Exo For You">
+      <th scope="row">14</th>
+      <td>Exo For You</td>
+      <td>Fiksi</td>
+      <td>EXO Squad</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Falling In Love">
+      <th scope="row">15</th>
+      <td>Falling In Love</td>
+      <td>Romantis</td>
+      <td>Erin Branscom</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Fahrenheit 451">
+      <th scope="row">16</th>
+      <td>Fahrenheit 451</td>
+      <td>Dystopia</td>
+      <td>Ray Bradbury</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Flora">
+      <th scope="row">17</th>
+      <td>Flora</td>
+      <td>Drama</td>
+      <td>Agnes Shintyas</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Game Over">
+      <th scope="row">18</th>
+      <td>Game Over</td>
+      <td>Thriller</td>
+      <td>Valerie Patkar</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Goodbye">
+      <th scope="row">19</th>
+      <td>Goodbye</td>
+      <td>Drama</td>
+      <td>Eriska Febriani</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Gustira">
+      <th scope="row">20</th>
+      <td>Gustira</td>
+      <td>Petualangan</td>
+      <td>Eko Ivano Winata</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Hafalan Sholat Delisa">
+      <th scope="row">21</th>
+      <td>Hafalan Sholat Delisa</td>
+      <td>Drama Religi</td>
+      <td>Tere liye</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Hanya Sekedar Mimpi">
+      <th scope="row">22</th>
+      <td>Hanya Sekedar Mimpi</td>
+      <td>romantis</td>
+      <td>Hujan Mimpi</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Hate List">>
+      <th scope="row">23</th>
+      <td>Hate List</td>
+      <td>Thriller</td>
+      <td>Jennifer Brown</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="I'm Fine">
+      <th scope="row">24</th>
+      <td>I'm Fine</td>
+      <td>Drama</td>
+      <td>Siti Umrotun</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Inestable">
+      <th scope="row">25</th>
+      <td>Inestable</td>
+      <td>Thriller</td>
+      <td>Eko Ivano Winata</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Ivanha Van Dijk">
+      <th scope="row">26</th>
+      <td>Ivanna Van Dijk</td>
+      <td>Drama</td>
+      <td>Risa Saraswati</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Jangan Takut">
+      <th scope="row">27</th>
+      <td>Jangan Takut</td>
+      <td>thriller</td>
+      <td>Irwan Abdollah</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Kalandra">
+      <th scope="row">28</th>
+      <td>Kalandra</td>
+      <td>Fantasi</td>
+      <td>Gentakiswara</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="Kisah Tanah Jawa">
+      <th scope="row">29</th>
+      <td>Kisah Tanah Jawa</td>
+      <td>Sejarah</td>
+      <td>Bonaventura D. Genta Mada Zidan (Mbah KJ) Hari Kurniawan</td>
+      <td class="status">Tersedia</td>
+    </tr>
+    <tr data-book="lafaz Cinta Groningen & Perasaan Cinta Yang Tak Pernah Padam">
+      <th scope="row">30</th>
+      <td>Lafaz Cinta Groningen & Perasaan Cinta Yang Tak Pernah Padam</td>
+      <td>Romantis</td>
+      <td>Sinta Yudisia</td>
+      <td class="status">Tersedia</td>
+    </tr>
+  </tbody>
+</table>
+
+<script>
+  const bookList = document.getElementById('bookList');
+  const borrowedBooks = JSON.parse(localStorage.getItem('borrowedBooks')) || [];
+
+  console.log(borrowedBooks);
+
+  document.addEventListener('DOMContentLoaded', () => {
+  borrowedBooks.forEach(borrowedBook => {
+    const bookRow = bookList.querySelector(`[data-book="${borrowedBook}"]`);
+    if (bookRow) {
+      bookRow.querySelector('.status').textContent = 'Tidak Tersedia';
+    }
+  });
+});
+</script>
+      </div>
+    </div>
+  </div>
+</div>
